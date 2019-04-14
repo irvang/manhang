40a9cbed9de296e2cf9599491e9503b672c3a927
Irving Angulo <irvang.manage@gmail.com>
2019-04-14 12:02:09 -0400

    Refactored function to fetch within listener.

--- views/js/modules/apiParamsListeners.js
+++ views/js/modules/apiParamsListeners.js
@@ -1,9 +1,12 @@
 import selectNewWord from './selectNewWord.js';
-import words from './state.js';
+import state from './state.js';
 
-
+// @desc Fetches data with parameters applied. 
+// The parameters are: difficulty, minLength, maxLength, since those seem to be the more intuitive
+// maxLength is added 1 (maxLength +=1) for a more intuitive GUI. i.e. 12  
+// normally fetches words of max length of 11, adding one to 12 (13) fetches words
+// of max lenght 12
 export default function apiParamsListeners() {
-  // export default function apiParamsListeners(word, chooseWord, alphabetSpansListeners, selectNewWord) {
 
   let selectDifficulty = document.querySelector('#difficulty');
   let selectMinLength = document.querySelector('#min-length');
@@ -13,32 +16,27 @@ export default function apiParamsListeners() {
 
   sectionParams.addEventListener('change', evt => {
 
+    const difficulty = parseInt(selectDifficulty.value);
+    const minLength = parseInt(selectMinLength.value);
 
-    fetchDataAndUpdate(
-      parseInt(selectDifficulty.value),
-      parseInt(selectMinLength.value),
+    /* Adding one to make interface more intuitive for user. */
+    const maxLength = parseInt(selectMaxLength.value) + 1;
 
-      /* Adding one to make interface more intuitive for user. */
-      parseInt(selectMaxLength.value) + 1);
-  });
-}
+    //fetch data with new parameters
+    fetch(`/words/${difficulty}/${minLength}/${maxLength}`)
+      .then(function (response) {
+        if (response.ok) {
+          // console.log(typeof response)
+          return response.json();//a promise, convert to object
+        }
+      })
+      .then(bodyAsJson => {
 
-function fetchDataAndUpdate(difficulty, minLength, maxLength) {
-  //should update
-  return fetch(`/words/${difficulty}/${minLength}/${maxLength}`)
-    .then(function (response) {
-      if (response.ok) {
-        // console.log(typeof response)
-        return response.json();//a promise, convert to object
-      }
-    })
-    .then(bodyAsJson => {
+        state.ALL_WORDS = [...bodyAsJson];
 
-      state.ALL_WORDS = bodyAsJson;
-      //may have to restructure so that gui loads before all the response
-      //====Starts game after receiving array
-      selectNewWord();// passes as ALL_WORDS
-      // console.log(typeof bodyAsJson);// passes as ALL_WORDS
-    })
-    .catch(error => console.log('ERROR: \n', error));
+        // udpates/restarts game with the selected parameters
+        selectNewWord();// passes as ALL_WORDS
+      })
+      .catch(error => console.log('ERROR: \n', error));
+  })
 }
