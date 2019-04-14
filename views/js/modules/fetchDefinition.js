5db3a79eb6aa5af015cd2d1e0c114d3e814e4fd2
Irving Angulo <irvang.manage@gmail.com>
2019-04-13 21:58:31 -0400

    Removed unused fetch. Redirection already happening on background. It is cleaner and better.

--- views/js/modules/fetchDefinition.js
+++ views/js/modules/fetchDefinition.js
@@ -1,7 +1,4 @@
 
-let controller = new AbortController();
-let signal = controller.signal;
-
 
 export default function (word) {
   return fetch(`/api/dictionaries/oxford/${word}`)
@@ -41,7 +38,6 @@ function showDefinition(bodyAsJson) {
   definitions.forEach((elm, i) => {
     definitionUl.innerHTML += `<li>${elm}</li>`;
   });
-  console.log(definitionUl)
 
   // source if there is a provider
   if (provider) { definitionsDiv.innerHTML = "<b>Source:</b> " + provider; }
@@ -50,7 +46,6 @@ function showDefinition(bodyAsJson) {
   definitionsSection.append(definitionUl);
   definitionsSection.append(definitionsDiv);
 
-  console.log(provider, definitions);
 }
 
 export function clearDefinitionsSection() {
@@ -61,35 +56,4 @@ export function clearDefinitionsSection() {
   definitionH3.innerHTML = '';
   definitionUl.innerHTML = '';
   definitionsDiv.innerHTML = '';
-}
-
-//NOW UNUSED, REDIRECTING ON SERVER
-function requestMerrian(word) {
-  return fetch(`/api/dictionaries/merrian/${word}`)
-    .then(function (response) {
-
-
-      //if word is found, will be 200, else will be 204
-      if (response.status === 200) {
-        return response.json();//a promise, convert to object
-      }
-      // else {
-
-      //   console.log("at end merrian no def")
-      //   //if here, show NO definition message on page
-      //   return {
-      //     provider: 'no provider',
-      //     definitions: ['No definition found']
-      //   }
-
-      // }
-    })
-    .then(bodyAsJson => {
-
-      //if here, show definition on page
-      console.log("at end merrian", bodyAsJson)
-
-      showDefinition(bodyAsJson);
-    })
-    .catch(error => console.log('ERROR: \n', error));
 }
